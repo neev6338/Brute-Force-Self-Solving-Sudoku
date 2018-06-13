f1020af04ccb455f85bfe237fac60f51118a7392
Bhargav Singh <bhargavsingh482@gmail.com>
2018-06-13 19:49:03 +0200

    Upload EnableDisable Function

---
+++ EnableDisable.js
@@ -0,0 +1,8 @@
+function disableall(){
+	document.getElementById("randombtn").disabled = true;
+	document.getElementById("bruteforce").disabled = true;
+}
+function enableall(){
+	document.getElementById("randombtn").disabled = false;
+	document.getElementById("bruteforce").disabled = false;
+}
